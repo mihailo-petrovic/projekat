e0052a97ff931e3d70c88d24f9b5c1b312992095
kamilica <mihailopetrovic995@gmail.com>
2021-02-20 16:10:56 +0100

    Registration and login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
+// Variables
 const regLink = document.getElementById("regLink");
 const regForm = document.getElementById("regForm");
 const logForm = document.getElementById("logForm");
 const login = document.getElementById("login");
 const registration = document.getElementById("registration");
+const regUserInput = document.getElementById("regUsername");
+const regPass1Input = document.getElementById("regPassword1");
+const regPass2Input = document.getElementById("regPassword2");
+const regMessage = document.getElementById("regMessage");
+const logMessage = document.getElementById("logMessage");
+const logUserInput = document.getElementById("username");
+const logUserPass = document.getElementById("password");
 
+let users, user;
+
+// Listeners
 regLink.addEventListener("click", () => {
   logForm.classList.add("disNone");
   regForm.classList.remove("disNone");
@@ -11,13 +22,111 @@ regLink.addEventListener("click", () => {
 
 registration.onsubmit = (e) => {
   e.preventDefault();
+  registerUser();
 };
 
 login.onsubmit = (e) => {
   e.preventDefault();
-//   window.location = "main.html";
+  loginUser();
 };
 
-let user = {};
-let users = [];
-let loggedUser = {};
+// Functions
+function registerUser() {
+  let username = regUserInput.value;
+  let pass1 = regPass1Input.value;
+  let pass2 = regPass2Input.value;
+
+  let userReg = /[a-zA-Z]{5,20}/;
+  let passReg = /(?=.*[0-9]){8,30}/;
+
+  // Testing user entries
+  if (!userReg.test(username)) {
+    regMessage.innerText = "Username needs to be at least 5 characters";
+    regMessage.classList.remove("disNone");
+    regUserInput.style.border = "1.5px solid rgb(161, 17, 17)";
+    return;
+  }
+  if (!passReg.test(pass1)) {
+    regMessage.innerText =
+      "Password needs to contain 8 characters and one digit";
+    regMessage.classList.remove("disNone");
+    regPass1Input.style.border = "1.5px solid rgb(161, 17, 17)";
+    regPass2Input.style.border = "1.5px solid rgb(161, 17, 17)";
+    return;
+  }
+  if (pass1 !== pass2) {
+    regMessage.innerText = "Passwords don't match each other";
+    regMessage.classList.remove("disNone");
+    regPass1Input.style.border = "1.5px solid rgb(161, 17, 17)";
+    regPass2Input.style.border = "1.5px solid rgb(161, 17, 17)";
+    return;
+  }
+  if (localStorage.getItem("projectTMDBUsers")) {
+    users = JSON.parse(localStorage.getItem("projectTMDBUsers"));
+    let exists = users.find((user) => user.username === username);
+    if (exists == undefined) {
+      pushUser(username, pass2);
+      clearForms();
+    } else {
+      regMessage.innerText = "Username already exists";
+      regMessage.classList.remove("disNone");
+    }
+  } else {
+    users = [];
+    pushUser(username, pass2);
+    clearForms();
+  }
+}
+
+function pushUser(username, password) {
+  user = {
+    username: username,
+    password: password,
+    likedMovies: [],
+    watchLater: [],
+    ratedMovies: [],
+  };
+  users.push(user);
+  let usersJson = JSON.stringify(users);
+  localStorage.setItem("projectTMDBUsers", usersJson);
+
+  logForm.classList.remove("disNone");
+  regForm.classList.add("disNone");
+}
+
+function loginUser() {
+  let username = logUserInput.value;
+  let password = logUserPass.value;
+  let users = JSON.parse(localStorage.getItem("projectTMDBUsers"));
+
+  let obj = users.find((user) => username === user.username);
+  if (obj == undefined) {
+    logMessage.innerText = "No such user";
+    logMessage.classList.remove("disNone");
+    return;
+  }
+  if (password != obj.password) {
+    logMessage.innerText = "Incorrect password!";
+    logMessage.classList.remove("disNone");
+    return;
+  }
+
+  if (password === obj.password) {
+    clearForms();
+    localStorage.setItem("loggedInTMDBUser", JSON.stringify(obj));
+    window.location = "../main.html";
+  }
+}
+
+function clearForms(){
+  logMessage.classList.add("disNone");
+  regMessage.classList.add("disNone");
+  regUserInput.value = '';
+  regPass1Input.value = '';
+  regPass2Input.value = '';
+  logUserInput.value = '';
+  logUserPass.value = '';
+  regUserInput.style.border = 'none';
+  regPass1Input.style.border = 'none';
+  regPass2Input.style.border = 'none';
+}
--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
 const container = document.getElementById("cont");
 
-fetch(
-  "https://api.themoviedb.org/4/search/movie?api_key=4809e2c8d7aced296b24cc62c56871c4&query=pirates"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.results[2].id);
-    console.log(data.results[2].original_title);
-    console.log(data.results[2].overview);
-    console.log(data.results[2].title);
-    container.innerHTML += `
-      <img class='logo_small' src='https://image.tmdb.org/t/p/w300${data.results[2].poster_path}'></img>
-    `
-    for (let i = 1; i <= data.total_pages; i++) {
-      fetch(
-        `https://api.themoviedb.org/4/search/movie?api_key=4809e2c8d7aced296b24cc62c56871c4&query=pirates&page=${i}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);          
-        });
-    }
-  });
+let user = localStorage.getItem("loggedInTMDBUser");
+
+console.log(JSON.parse(user));
